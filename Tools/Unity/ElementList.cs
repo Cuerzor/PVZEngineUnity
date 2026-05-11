4664cc3c3babbe81d1d00775eed46beb74111e57
Cuerzor58 <773785114@qq.com>
2026-05-11 19:33:46 +0800

    refactor: 整理命名空间。

--- Tools/Unity/ElementList.cs
+++ Tools/Unity/ElementList.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MVZ2.UI
+namespace Tools
 {
     public class ElementList : MonoBehaviour
     {
--- Tools/Unity/ElementListUI.cs
+++ Tools/Unity/ElementListUI.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace MVZ2.UI
+namespace Tools
 {
     public class ElementListUI : MonoBehaviour
     {
